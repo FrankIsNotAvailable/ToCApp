3a5e254169c306b1da287e1e8cdf7196811699a2
zeeZaaAa <supp.chakorn@gmail.com>
2026-08-13 22:09:53 +0700

    chore: set up initial project

--- src/layouts/RootLayout.svelte
+++ src/layouts/RootLayout.svelte
@@ -1,8 +1,18 @@
 <script>
 	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
     
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>TOC | KMITL</title>
+	<meta
+		name="description"
+		content="Application for Theory of Computation assignment."
+	/>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
 <div class="flex min-h-screen flex-col font-main">
 	{@render children()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
+	import RootLayout from '$layouts/RootLayout.svelte';
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children()}
+<RootLayout>
+	{@render children()}
+</RootLayout>
