22f0ea9a2e595a65fb5d55c37a8550c27c011572
FrankIsNotAvailable <jagreeto2549@gmail.com>
2026-08-28 18:13:01 +0700

    Little CSS adjust
    - Changed CSS into tailwind CSS.
    - Moved background into RootLayout file.
    -Jam link: https://jam.dev/c/6627b29b-96bd-4238-bc4c-da8eee22abc8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,12 @@
 	import PersonalInfo from '$components/PersonalInfo.svelte';
 </script>
 
-<div class="background"></div>
-
-<div class="page">
+<div class="flex h-screen flex-col">
 	<header>
 		<NavBar />
 	</header>
 
-	<div class="content">
+	<div class="min-h-0 flex-1 overflow-y-auto">
 		<div class="min-h-full flex items-center justify-center py-[1rem]">
 			<!-- <HomeDataAvaliable /> -->
 			<PersonalInfo personalTitle="Personal Information Entry" />
