30c961f3d33899b826d76f1ec8de2cbc439f57f5
FrankIsNotAvailable <jagreeto2549@gmail.com>
2026-09-06 21:08:15 +0700

    Landing page
    - Landing page was made.
    - Dedicated navbar for this page was created.
    - A reusable component was created.
    - Animation was added.
    - A lot of new .svg + .txt file was added.
    - Jam link: https://jam.dev/c/0a5cfee9-8deb-40fe-bd47-03fa9a5a4f4a

---
+++ src/components/SupportedData.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let picture = "";
+    export let pictureAlt = "";
+    export let title = "";
+    export let description = "";
+    export let pattern = "";
+</script>
+
+<div class='flex h-full w-full flex-col items-center
+            rounded-[17px]
+            px-[4rem] pt-[4rem] pb-[1.5rem]
+            font-[family-name:var(--inter-font)]'>
+    
+    <img src={picture} alt={pictureAlt} class='w-[10rem] h-[10rem]'/>
+
+    <div class='text-[2.25rem] pt-[2rem]'>
+        {title}
+    </div>
+
+    <div class='text-[1.125rem] text-[#656565] text-center leading-loose
+                pt-[1rem] h-[20%]'>
+        {description}
+    </div>
+
+    <div class='text-[1.5rem] pt-[2rem] h-[20%]'>
+        Pattern: {pattern}
+    </div>
+</div>
