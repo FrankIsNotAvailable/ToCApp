d24ea4102821e459cfa4ed58fe6f90711f9be3f5
zeeZaaAa <supp.chakorn@gmail.com>
2026-09-09 23:04:49 +0700

    fix: replace button with GlassyButton component for create data action

--- src/components/LandingNavBar.svelte
+++ src/components/LandingNavBar.svelte
@@ -8,10 +8,10 @@
         <a href='#about-us' class="hover:underline active:translate-y-[1px]">about us</a>
         <a href='#features' class="hover:underline active:translate-y-[1px]">features</a>
         <a href='#more-info' class="hover:underline active:translate-y-[1px]">more info</a>
-        <div class='bg-white/20 border border-white/35 rounded-[17px] px-[1.25rem] 
+        <a href='/login' class='bg-white/20 border border-white/35 rounded-[17px] px-[1.25rem] 
                     py-[0.5rem] shadow-[0px_4px_6px_0px_rgba(0,0,0,0.25)]
                     active:translate-y-[2px] active:shadow-[0_2px_0_#999]'>
-            <a href='/login'>log in</a>
-        </div>
+            <span >log in</span>
+        </a>
     </div>
 </div>
