5f2f8ff0cf2a03b77f5f952f8f61ced64d677735
zeeZaaAa <supp.chakorn@gmail.com>
2026-09-09 12:05:40 +0700

    feat: refine NavBar component styles for sticky at the top

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -16,33 +16,33 @@
 </script>
 
 <div
-    class="navBar z-1 flex shrink-0 items-center
-            justify-between border-b border-white/35 bg-[var(--navbar-background)]/20 px-[0.5rem]
-            py-[0.5rem] font-[family-name:var(--navbar-font)] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.2)]
+    class="navBar z-1 flex shrink-0 items-center sticky top-0
+            justify-between border-b border-white/35 bg-(--navbar-background)/20 px-2
+            py-2 font-(family-name:--navbar-font) shadow-[0px_4px_20px_0px_rgba(0,0,0,0.2)]
             backdrop-blur-[5px]"
 >
-    <div class="navBarTitle px-[2rem] text-[3.5rem]">Obscura+</div>
+    <div class="navBarTitle px-8 text-[3.5rem]">Obscura+</div>
 
     <AuthGuard requireAuth={true}>
         <div
-            class="navBarButtons flex items-center justify-between gap-[1rem] px-[2rem]
-                font-[family-name:var(--inter-font)] text-[1.25rem]"
+            class="navBarButtons flex items-center justify-between gap-4 px-8
+                font-(family-name:--inter-font) text-[1.25rem]"
         >
             {displayName}
 
-            <button class="button h-[3rem] w-[3rem]">
+            <button class="button h-12 w-12">
                 <img src={profile} alt="profile" class="navBarCreatePage" />
             </button>
         </div>
 
         {#snippet fallback()}
             <div
-                class="navBarButtons flex items-center justify-between gap-[1rem] px-[2rem]
-                    font-[family-name:var(--inter-font)] text-[1.25rem]"
+                class="navBarButtons flex items-center justify-between gap-4 px-8
+                    font-(family-name:--inter-font) text-[1.25rem]"
             >
                 {'Guest'}
 
-                <div class="h-[3rem] w-[3rem]">
+                <div class="h-12 w-12">
                     <img src={profile} alt="profile" class="navBarCreatePage" />
                 </div>
             </div>
