871aacd516c746733caeeea738b1a81de8368b8e
FrankIsNotAvailable <jagreeto2549@gmail.com>
2026-09-10 14:50:35 +0700

    Guest data masking page
    - Added guest masking page.
    - Made a reusable FieldCard for both guest page and logged user page, set its parameters and you're all set [type = {1} for input, type = {2} for output. + use width as width = "x%".].
    - Button component added, needs onclick event handler. <<< Please help
    - Jam link: https://jam.dev/c/623d0629-cf9a-487b-95ef-323c821a31c5

---
+++ src/components/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let svg;
+    export let svgAlt;
+    export let title;
+    export let text;
+    export let border;
+    export let background;
+</script>
+
+<button class='flex justify-center items-center border rounded-lg px-[1.5rem] py-[0.5rem]
+            font-medium gap-[0.5rem] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.5)]
+            {border} {background} {text}'>
+    <img src={svg} alt={svgAlt} class="w-[1.25rem] h-[1.25rem]"/>
+    {title}
+</button>
+
+<!-- 
+    !!! Add whatever onclick event for each type of button !!!
+    !!! Currently button has copy + mask. Check it from title !!!
+    !!! title = Mask Text -> mask data !!!
+    !!! title = Copy to Clipboard -> copy !!!
+-->
