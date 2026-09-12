ffc900a69827a611c399af77980081f080b90d55
Jirapol-Pongthong <jirapol.phongtong@gmail.com>
2026-09-12 18:23:34 +0700

    feat: add deletion confirmation popup component

---
+++ src/routes/deletion/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import NavBar from '$components/NavBar.svelte';
+    import Deletion from '$components/Deletion.svelte';
+
+    let open = $state(true);
+</script>
+
+<NavBar/>
+<div class="flex flex-1 items-center justify-center mb-[4rem]">
+    <button type="button" class="rounded-full bg-black px-4 py-2 text-white" onclick={() => (open = true)}>
+        Open deletion popup
+    </button>
+    <Deletion {open} onCancel={() => (open = false)} onConfirm={() => (open = false)} />
+</div>
